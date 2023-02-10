45dff0e98fbaf63c00e5f021ae2b16650acea073
daniela marques <dd978452@gmail.com>
2023-02-10 17:59:04 -0300

    Estrutura de Repetição - For

--- Program.cs
+++ Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Globalization;
+
+namespace For3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int N = int.Parse(Console.ReadLine());
+
+            int[] A = new int[N];
+            int[] B = new int[N];
+
+            string[] valores = Console.ReadLine().Split(' ');
+            for (int i = 0; i < N; i++)
+            {
+                A[i] = int.Parse(valores[i]);
+            }
+
+            valores = Console.ReadLine().Split(' ');
+            for (int i = 0; i < N; i++)
+            {
+                B[i] = int.Parse(valores[i]);
+            }
+
+            int[] C = new int[N];
+            for (int i = 0; i < N; i++)
+            {
+                C[i] = A[i] + B[i];
+            }
+            for (int i = 0; i < N; i++)
+            {
+                Console.Write(C[i] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
